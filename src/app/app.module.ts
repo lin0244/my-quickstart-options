d8dde44c051a2493f079f39e63687b64aaebcd6d
zhiq <865077695@qq.com>
2017-08-31 16:42:55 +0800

    一些常用场景的示例写法

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
+import { TestSubComponent } from './test-sub/test-sub.component';
+import { TestHttpComponent } from './test-http/test-http.component';
 
+import { HttpService } from './service/http.service';
+import { TestSubjectComponent } from './test-subject/test-subject.component';
 @NgModule({
   declarations: [
     AppComponent,
+    TestSubComponent,
+    TestHttpComponent,
+    TestSubjectComponent,
   ],
   imports: [
     BrowserModule,
@@ -21,7 +28,7 @@ import { appRoutes } from './app.routes';
     NgZorroAntdModule.forRoot(),
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
