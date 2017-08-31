b99ca741754f36318294640e6344ad30267a3fef
zhiq <865077695@qq.com>
2017-08-31 16:42:39 +0800

    http（get、post）示例

---
+++ src/app/service/user-model.ts
@@ -0,0 +1,11 @@
+export class User {
+  id: number;
+  postTitle: string;
+  content: string;
+  author: string;
+  postTime: Date;
+  readTimes: number;
+  commentTimes: number;
+  nickName: string;
+  originalURL: string;
+}
